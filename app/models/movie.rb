01512a04bcdd6339623f68410e33f58058bc0a41
mkskovalev <maxonemore@ya.ru>
2023-02-17 09:40:56 +0700

    add actor model with specs

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,6 +6,8 @@ class Movie
   field :image, type: String
   field :raiting, type: Float
 
+  has_and_belongs_to_many :actors
+
   validates :title, uniqueness: true
   validates :title, :image, :raiting, presence: true
 end
